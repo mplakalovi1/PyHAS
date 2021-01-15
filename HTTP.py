4dcd7bbd3c1e2de931aff60a75dfc119cbcbdeab
Matej <matej.plakalovic@gmail.com>
2021-01-15 02:39:55 +0100

    Added property decorator in HTTP module.

--- HTTP.py
+++ HTTP.py
@@ -5,22 +5,27 @@ logging.basicConfig(filename='pyHAS.log', level=logging.INFO, format='%(asctime)
 
 class Http:
 
-    def __init__(self, url, TIMEOUT=5):
+    def __init__(self, url: str, timeout: float):
         logging.info("Request sent (request url: '{}')".format(url))  # date & time of sending request (in local time)
-        self.response = requests.get(url, timeout=TIMEOUT)  # response object
+        self.response = requests.get(url, timeout=timeout)  # response object
         logging.info("Response received.")  # date & time of response (in local time)
 
-    def getsize(self):  # response size in bytes
+    @property
+    def size(self):  # response size in bytes
         return self.response.headers['Content-Length']
 
-    def getrsptime(self):  # response time in seconds
+    @property
+    def response_time(self):  # response time in seconds
         return self.response.elapsed.total_seconds()
 
-    def usersbandwidth(self):  # user's bandwidth [bps]
-        return float(self.getsize()) * 8 / float(self.getrsptime())
+    @property
+    def users_bandwidth(self):  # user's bandwidth [bps]
+        return float(self.size) * 8 / float(self.response_time)
 
-    def getcontent(self):  # content of the response, in unicode
+    @property
+    def content(self):  # content of the response, in unicode
         return self.response.text
 
+    @property
     def successful(self):  # check if response code is 200 (check if response is successful)
         return self.response.status_code == 200
