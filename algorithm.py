ce2328d89d407e6c7db0d0ba8bbf3aeb2c6a16eb
Matej <matej.plakalovic@gmail.com>
2021-01-15 07:22:04 +0100

    Added base class Algorithm, classes Algorithm 1 and Algorithm are inherited from base class!

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,73 @@
+import logging
+
+
 class Algorithm:
-    def __init__(self, bandwidths: list):
-        self.bandwidths = bandwidths  # available bandwidths for segments
+    def __init__(self, bitrates: list):
+        self.__bitrates = bitrates  # available bit rates for segments
+        self.__buffer: float = 0
+        self.__previous = list()  # list of all previous bit rates determined by algorithm
+        self.__usersbandwidth = list()  # list of user's bandwidth
 
-    def algorithm_1(self, usersbandwidth):  # multiple hops if it's possible
-        self.bandwidths.sort(reverse=True)  # bandwidths sorted in descending order
+    @property
+    def bitrates(self):
+        return self.__bitrates
 
-        for segment in self.bandwidths:  # finding best possible segment's quality considering user's bandwidth
-            if segment <= usersbandwidth:
-                return segment
+    @property
+    def buffer(self):
+        return self.__buffer
 
-        return self.bandwidths[len(self.bandwidths) - 1]  # if user's bw is under all segments bw's choose the smallest quality
-
-    def algorithm_2(self, userbandwidth, previous):  # for smoother transitions (one hop up or down)
-        index = self.bandwidths.index(previous)  # index of previous segment's bw
-
-        if userbandwidth < previous:
-            if index == 0:
-                return self.bandwidths[0]  # choose smallest if previous was also smallest
-            else:
-                return self.bandwidths[index - 1]
-        elif userbandwidth == previous:
-            return self.bandwidths[index]
+    @buffer.setter
+    def buffer(self, value: float):
+        self.__buffer = value
+        if self.__buffer < 0:
+            logging.info("BUFFER STALLED {} seconds".format(abs(self.__buffer)))
+            self.__buffer = 0
         else:
-            if index == len(self.bandwidths) - 1:
-                return self.bandwidths[len(self.bandwidths) - 1]
+            logging.info("Buffer = {} sec".format(self.__buffer))
+
+    @property
+    def previous(self):
+        return self.__previous
+
+    @previous.setter
+    def previous(self, value: int):
+        self.__previous.append(value)
+
+    @property
+    def usersbandwidth(self):
+        return self.__usersbandwidth
+
+    @usersbandwidth.setter
+    def usersbandwidth(self, value: float):
+        self.__usersbandwidth.append(value)
+
+
+class Algorithm1(Algorithm):  # multiple hops if it's possible
+    def alg1(self):
+        for bitrate in range(len(self.bitrates), -1, -1):  # finding best possible bit rate considering user's bandwidth
+            if bitrate <= self.usersbandwidth[-1]:
+                self.previous = bitrate
+                return
+        self.previous = self.bitrates[0]  # if user's bw is under all bit rates choose the smallest quality
+
+
+class Algorithm2(Algorithm):  # for smoother transitions (one hop up or down)
+    def alg2(self):
+        index = self.bitrates.index(self.previous[-1])  # index of previous segment's bit rate
+
+        if self.usersbandwidth[-1] < self.previous[-1]:
+            if index == 0:
+                self.previous = self.bitrates[index]  # choose smallest if previous was also smallest
+                return
             else:
-                return self.bandwidths[index + 1]
+                self.previous = self.bitrates[index - 1]
+                return
+        elif self.usersbandwidth[-1] == self.previous[-1]:
+            self.previous = self.bitrates[index]
+            return
+        else:
+            if index == len(self.bitrates) - 1:
+                self.previous = self.bitrates[index]
+                return
+            else:
+                self.previous = self.bitrates[index + 1]
